613cbda6727ad88dd5e617a20923d9139b00b72c
glpzghoo <my.as.glpzghoo@gmail.com>
2025-08-11 06:06:44 +0800

    update getUserById and CurrentUser queries

--- src/graphql/resolvers/queries/get-user-by-id.ts
+++ src/graphql/resolvers/queries/get-user-by-id.ts
@@ -1,23 +1,28 @@
 import { eq } from 'drizzle-orm';
 import { db } from '../../../db/client';
 import Catch_Error from '../../../utils/GraphqlError';
-import { users } from '../../../db/schema/user';
 import { taskApplications } from '../../../db/schema/task-applications';
+import { users } from '../../../db/schema';
 
 const getUserById = async (_: unknown, { id }: { id: string }) => {
   try {
-    const user = await db.select().from(users).where(eq(users.id, id)).limit(1);
+    const user = await db.query.users.findFirst({
+      where: eq(users.id, id),
+      with: {
+        postedTasks: true,
+      },
+    });
     const UserTasks = await db
       .select()
       .from(taskApplications)
       .where(eq(taskApplications.helperId, id));
-
-    if (user.length === 0) {
+    if (!user) {
       throw new Error('Хэрэглэгч олдсонгүй!');
     }
 
-    return { user: user[0], taskApplications: UserTasks };
+    return { user, taskApplications: UserTasks };
   } catch (err) {
+    console.error(err);
     return Catch_Error(err);
   }
 };
